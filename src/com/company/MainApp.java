be5b22583301c4dce5035c27418344dc5ad2e55a
Marcin4433 <marcin.bia4433@gmail.com>
2019-11-09 16:32:33 +0100

    < main application>

---
+++ src/com/company/MainApp.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.awt.*;
+import java.io.IOException;
+
+
+public class MainApp {
+    public static void main(String[] args) throws IOException {
+
+        Switch aSwitch=new Switch();
+        aSwitch.Choice();
+
+
+    }
+}
