02f7a97d133ff6e2813ded3fcc51257ebebd21bb
Marcin4433 <marcin.bia4433@gmail.com>
2019-11-09 16:46:06 +0100

    < class users >

---
+++ src/com/company/Users.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import java.io.FileNotFoundException;
+
+
+
+public class Users {
+
+    public void nextElement() throws FileNotFoundException {
+        File file = new File("./src/users.txt");
+        Scanner users = new Scanner((file));
+        StringTokenizer token;
+        Object tk;
+        while (users.hasNextLine()) {
+            token = new StringTokenizer(users.nextLine(), ",");
+
+            while (token.hasMoreElements()) {
+                //System.out.println("Token = " + token.nextToken());
+                tk = token.nextElement();
+                System.out.println("Token = " + tk);
+
+            }
+
+        }
+    }
+}
