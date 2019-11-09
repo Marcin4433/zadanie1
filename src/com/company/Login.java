01d7ed1fe3802ed5d5b3ef3888a339940d08478d
Marcin4433 <marcin.bia4433@gmail.com>
2019-11-09 16:42:24 +0100

    < class Login >

---
+++ src/com/company/Login.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner; // I use scanner because it's command line.
+
+public class Login {
+    public void run() throws FileNotFoundException {
+        Scanner scan = new Scanner (new File("./src/login.txt"));
+        Scanner keyboard = new Scanner (System.in);
+        String user = scan.nextLine();
+        String pass = scan.nextLine(); // looks at selected file in scan
+        System.out.println("Write a login");
+        String inpUser = keyboard.nextLine();
+        System.out.println("Write a Password");
+        String inpPass = keyboard.nextLine(); // gets input from user
+
+        if (inpUser.equals(user) && inpPass.equals(pass)) {
+            System.out.println("you are login correctly");
+
+        } else {
+            System.out.println("incorrect password - error ");
+        }
+    }
+}
